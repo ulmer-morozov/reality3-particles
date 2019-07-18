b885ed7d26bc38d7d980ee484def9349c88629fc
ulmer-morozov <ulmer.morozov@gmail.com>
2019-07-18 19:47:35 +0300

    fix title

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const plugins = [
     }
   }),
   new HtmlWebpackPlugin({
-    title: 'Typescript Webpack Starter',
+    title: 'reality3',
     template: '!!ejs-loader!src/index.html'
   }),
   new webpack.LoaderOptionsPlugin({
